40dc7d470836be54b12b57703b9fc1a3d69fac6b
coleberr36 <coleberr36@gmail.com>
2024-06-19 04:03:03 +0000

    Initial commit with Flask app and Netlify config

---
+++ static/script.js
@@ -0,0 +1,70 @@
+const keyboard = document.querySelector('.keyboard-container')
+const tiles = document.querySelectorAll('.tile')
+const tilesArray = Array.from(tiles)
+const wordList = ["apple", "banana", "cherry", "grape", "orange"];
+
+const randomWord = getRandomWord(wordList);
+
+let currentTile = 0;
+let currentRow = 0;
+
+
+function getRandomWord(list) {
+    const randomIndex = Math.floor(Math.random() * list.length);
+    return list[randomIndex];
+}
+
+function handle_word_submisson(randomWord) {
+    // get the word from the tiles
+
+    const currentRowTiles = tilesArray.slice(currentRow * 5, (currentRow * 5) + 5);
+
+    const user_word = currentRowTiles.map(tile => tile.innerHTML).join('')
+
+    currentRow++;
+    currentTile = 0;
+
+}
+
+function delete_letter() {
+
+    let tileToDelete = currentRow * 5 + currentTile;
+
+    if (currentTile > 0) {
+        tiles[tileToDelete - 1].innerHTML = "";
+        currentTile--;
+
+        console.log(currentTile)
+    }
+
+}
+
+// add an event if any clicks occur on the keyboard
+keyboard.addEventListener('click', (event) => {
+    // check if user clicks a key and which one
+    if (event.target.classList.contains('key')) {
+        if (event.target.innerHTML === "Enter") {
+            // ensuring that the user is on the final tile of the row
+            if (currentTile === 5) {
+                handle_word_submisson(randomWord)
+            }
+        }
+
+        else if (event.target.innerHTML === "Delete") {
+            delete_letter();
+        }
+
+        else {
+            // get the tile index by getting the current row and multiplying it by 5
+            // * 5 for each tile in the row then add the current tile
+            if (currentTile < 5) {
+                const tileIndex = currentRow * 5 + currentTile
+                tiles[tileIndex].innerHTML = event.target.innerHTML
+
+                currentTile++;
+
+            }
+        }
+    }
+});
+
